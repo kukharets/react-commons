96d89dff45b5097cc6bc04535a9dadf8bcad9203
Your Name <you@example.com>
2018-03-19 02:24:34 +0200

    empty react+webpack project for fic

---
+++ fic/fic-webapp/src/js/components/container/MainContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+class MainContainer extends Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <div id='main-fic'>
+                React clear page
+            </div>
+        );
+    }
+
+
+}
+export default MainContainer;
+
+
